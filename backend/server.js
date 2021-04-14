c3184d44ddb84184fcc9d0d2b63d8a5302b6ce78
jcbain <jamescbain@gmail.com>
2021-04-14 11:13:45 -0600

    update starting point of backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 8000;
 
-
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/books', (req, res) => {
 
